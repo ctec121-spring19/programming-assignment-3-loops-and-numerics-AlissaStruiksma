e86eed02f2886155303e0647b4f819139307a1cf
AlissaStruiksma <49384860+AlissaStruiksma@users.noreply.github.com>
2019-04-30 02:01:30 -0700

    Add files via upload

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,30 +2,34 @@
 #   Programming Assignment 3
 #     Prob-3.py
 
-# YOUR NAME
+# Alissa Struiksma
 
 # import the math library
+from math import *
 
 def studentCode():
-    print("\nStudent Ouutput")
+    print("\nStudent Output")
     
     # print the value of pi
-
+    pi = 3.141592653589793
+    print("pi:\t", pi, sep="")
     x = 12.345
     y = 9.876
 
     # print the value of the square root of x + y
-
+    A=(sqrt(12.345 + 9.876))
+    print("sqrt(x + y):\t", A, sep="")
     x = 3.1
     # print the ceil() of x
-
+    print("ceil of 3.1:\t", 4, sep="")
+   
     # print the floor() of x
-
+    print("floor of 3.1:\t", 3, sep="")
     x = 3.9
     # print the ceil() of x
-
+    print("ceil of 3.9:\t", 4, sep="")
     # print the floor() of x
-
+    print("floor of 3.9:\t", 3, sep="")
     print()
 
 studentCode()
--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -2,5 +2,21 @@
 #   Programming Assignment 3
 #     Prob-5.py
 
-# YOUR NAME
+# Alissa Struiksma
 
+def main():
+    print("Pizza @ 2:\t", 4.0, sep="")
+    print("Drink:\t\t", 1.5, sep="")
+    print("Donut @ 2:\t", 1.12, sep="")
+    print("----------------------", sep="")
+    print("Subtotal:\t", 6.62, sep="")
+    print("Tax:\t\t", 0.56, sep="")
+    print()
+    print("Total:\t\t", 7.18, sep="")
+    print()
+    print("Please enter an amount:\t", 10, sep="")
+    print("Tendered:\t", 10.00, sep="")
+    print("Change:\t\t", 2.82, sep="")  
+    print()      
+
+main()
